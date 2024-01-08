0e1fb6994b45f28e1c9d2103bf909d5a68508c1e
samorso <Samuel.Orso@unige.ch>
2024-01-08 10:09:22 +0100

    modify e.g. lmer

--- inst/examples/eg_lmer.R
+++ inst/examples/eg_lmer.R
@@ -1,11 +1,11 @@
 
 ## linear mixed-effects regression
+\dontrun{
 library(lme4)
 fit_lmm <- lmer(Reaction ~ Days + (Days | Subject), data = sleepstudy, REML = FALSE)
 fit_ib <- ib(fit_lmm)
 summary(fit_ib)
 ## correct for variances and correlation
-\dontrun{
 fit_ib <- ib(fit_lmm, extra_param = TRUE)
 summary(fit_ib)
 }
