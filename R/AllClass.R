7a88cba85173f983f5f3dfb630278b0be350f3ac
samorso <Samuel.Orso@unige.ch>
2025-09-04 14:38:33 +0200

    update

--- R/AllClass.R
+++ R/AllClass.R
@@ -2,178 +2,182 @@
 # Copyright (C) 2020 S. Orso, University of Geneva
 # All rights reserved.
 
-## Class definition for the package
+#' @import methods
+#' @importFrom methods setOldClass setClass setClassUnion className
+NULL
 
-## NOTE: due to compatibility issues between S3/S4 classes,
-##       we consider supplying the fitted model in a new slot.
-
+## ---- Base old-class registrations (S3/S4 interop) -------------------------
 setOldClass("betareg")
-#' @rdname Ib
-#' @description Fitted model by \code{betareg} from \pkg{betareg}
+setOldClass("summary.betareg")
+setOldClass("summary.glm")
+setOldClass("summary.lm")
+setOldClass("negbin")
+setOldClass("summary.negbin")
+setOldClass("nls")
+setOldClass("summary.nls")
+
+## lme4 old classes (helps R CMD check be explicit)
+setOldClass("summary.merMod")
+
+## ---- Documentation: Ib* classes (one page, many aliases) ------------------
+
+#' Fitted-model wrappers for the iterative bootstrap (\code{ib})
+#'
+#' S4 classes that wrap fitted model objects and attach extra metadata from the
+#' iterative bootstrap procedure.
+#'
+#' Members include wrappers around models from \pkg{betareg}, \pkg{stats},
+#' \pkg{lme4}, \pkg{MASS}, and \pkg{VGAM}.
+#'
+#' @slot object  The original fitted model object, corrected by \code{ib}.
+#' @slot ib_extra A \code{list} of extra metadata from \code{ib}.
+#'
+#' @return Objects of the respective S4 classes.
+#'
+#' @details
+#' These classes are light wrappers: they store the corrected fitted object in
+#' \code{@object} (see \code{\link{getObject}}) and additional information in
+#' \code{@ib_extra} (see \code{\link{getExtra}}).
+#'
+#' @seealso \code{\link{getObject}}, \code{\link{getExtra}},
+#' \linkS4class{Ib}, \linkS4class{SummaryIb}
+#'
+#' @name Ib-class
+#' @aliases
+#' Ib-class
+#' IbBetareg-class IbGlm-class IbLm-class IbLmer-class IbGlmer-class
+#' IbNegbin-class IbNls-class IbVglm-class
+NULL
+
+#' @rdname Ib-class
 #' @export
 setClass("IbBetareg",
-         slots = list(
-           object = "betareg",
-           ib_extra = "list"))
+         slots = list(object = "betareg", ib_extra = "list"))
 
-setOldClass("summary.betareg")
-#' @rdname SummaryIb
-#' @description summary of class \code{summary.betareg} from \pkg{betareg}
-#' @export
-setClass("SummaryIbBetareg",
-         slots = list(
-           summ = "summary.betareg",
-           ib_extra = "list"))
-
-
-#' @rdname Ib
-#' @description fitted model by \code{glm} from \pkg{stats}
+#' @rdname Ib-class
 #' @export
 setClass("IbGlm",
-         slots = list(
-           object = "glm",
-           ib_extra = "list"))
+         slots = list(object = "glm", ib_extra = "list"))
 
-setOldClass("summary.glm")
-#' @rdname SummaryIb
-#' @description summary of class \code{summary.glm} from \pkg{stats}
-#' @export
-setClass("SummaryIbGlm",
-         slots = list(
-           summ = "summary.glm",
-           ib_extra = "list"))
-
-#' @rdname Ib
-#' @description fitted model by \code{glmer} from \pkg{lme4}
-#' @export
-setClass("IbGlmer",
-         slots = list(
-           object = className("glmerMod","lme4"),
-           ib_extra = "list"))
-
-setOldClass("summary.merMod")
-#' @rdname SummaryIb
-#' @description summary of class \code{summary.merMod} from \pkg{lme4}
-#' @export
-setClass("SummaryIbGlmer",
-         slots = list(
-           summ = "summary.merMod",
-           ib_extra = "list"))
-
-#' @rdname Ib
-#' @description fitted model by \code{lm} from \pkg{stats}
+#' @rdname Ib-class
 #' @export
 setClass("IbLm",
-         slots = list(
-           object = "lm",
-           ib_extra = "list"))
+         slots = list(object = "lm", ib_extra = "list"))
 
-setOldClass("summary.lm")
-#' @rdname SummaryIb
-#' @description summary of class \code{summary.lm} from \pkg{stats}
-#' @export
-setClass("SummaryIbLm",
-         slots = list(
-           summ = "summary.lm",
-           ib_extra = "list"))
-
-#' @rdname Ib
-#' @description fitted model by \code{lmer} from \pkg{lme4}
+#' @rdname Ib-class
 #' @export
 setClass("IbLmer",
-         slots = list(
-           object = className("lmerMod","lme4"),
-           ib_extra = "list"))
+         slots = list(object = className("lmerMod", "lme4"),
+                      ib_extra = "list"))
 
-# setOldClass("summary.merMod")
-#' @rdname SummaryIb
-#' @description summary of class \code{summary.merMod} from \pkg{lme4}
+#' @rdname Ib-class
 #' @export
-setClass("SummaryIbLmer",
-         slots = list(
-           summ = "summary.merMod",
-           ib_extra = "list"))
+setClass("IbGlmer",
+         slots = list(object = className("glmerMod", "lme4"),
+                      ib_extra = "list"))
 
-setOldClass("negbin")
-#' @rdname Ib
-#' @description fitted model by \code{glm.nb} from \pkg{MASS}
+#' @rdname Ib-class
 #' @export
 setClass("IbNegbin",
-         slots = list(
-           object = "negbin",
-           ib_extra = "list"))
+         slots = list(object = "negbin", ib_extra = "list"))
 
-setOldClass("summary.negbin")
-#' @rdname SummaryIb
-#' @description summary of class \code{summary.negbin} from \pkg{MASS}
-#' @export
-setClass("SummaryIbNegbin",
-         slots = list(
-           summ = "summary.negbin",
-           ib_extra = "list"))
-
-setOldClass("nls")
-#' @rdname Ib
-#' @description fitted model by \code{nls} from \pkg{stats}
+#' @rdname Ib-class
 #' @export
 setClass("IbNls",
-         slots = list(
-           object = "nls",
-           ib_extra = "list"))
+         slots = list(object = "nls", ib_extra = "list"))
 
-setOldClass("summary.nls")
-#' @rdname SummaryIb
-#' @description summary of class \code{summary.nls} from \pkg{stats}
-#' @export
-setClass("SummaryIbNls",
-         slots = list(
-           summ = "summary.nls",
-           ib_extra = "list"))
-
-#' @rdname Ib
-#' @description fitted model by \code{vglm} from \pkg{VGAM}
+#' @rdname Ib-class
 #' @export
 setClass("IbVglm",
-         slots = list(
-           object = className("vglm","VGAM"),
-           ib_extra = "list"))
+         slots = list(object = className("vglm", "VGAM"),
+                      ib_extra = "list"))
 
-#' @rdname SummaryIb
-#' @description summary of class \code{summary.vglm} from \pkg{VGAM}
+## ---- Documentation: SummaryIb* classes (one page, many aliases) -----------
+
+#' Summaries for \code{ib} wrappers
+#'
+#' S4 classes that wrap summary objects corresponding to the \code{Ib*} classes.
+#'
+#' @slot summ    The \code{summary.*} object.
+#' @slot ib_extra A \code{list} of extra metadata from \code{ib}.
+#'
+#' @seealso \linkS4class{SummaryIb}, \linkS4class{Ib}
+#'
+#' @name SummaryIb-class
+#' @aliases
+#' SummaryIb-class
+#' SummaryIbBetareg-class SummaryIbGlm-class SummaryIbLm-class
+#' SummaryIbLmer-class SummaryIbGlmer-class SummaryIbNegbin-class
+#' SummaryIbNls-class SummaryIbVglm-class
+NULL
+
+#' @rdname SummaryIb-class
+#' @export
+setClass("SummaryIbBetareg",
+         slots = list(summ = "summary.betareg", ib_extra = "list"))
+
+#' @rdname SummaryIb-class
+#' @export
+setClass("SummaryIbGlm",
+         slots = list(summ = "summary.glm", ib_extra = "list"))
+
+#' @rdname SummaryIb-class
+#' @export
+setClass("SummaryIbLm",
+         slots = list(summ = "summary.lm", ib_extra = "list"))
+
+#' @rdname SummaryIb-class
+#' @export
+setClass("SummaryIbLmer",
+         slots = list(summ = "summary.merMod", ib_extra = "list"))
+
+#' @rdname SummaryIb-class
+#' @export
+setClass("SummaryIbGlmer",
+         slots = list(summ = "summary.merMod", ib_extra = "list"))
+
+#' @rdname SummaryIb-class
+#' @export
+setClass("SummaryIbNegbin",
+         slots = list(summ = "summary.negbin", ib_extra = "list"))
+
+#' @rdname SummaryIb-class
+#' @export
+setClass("SummaryIbNls",
+         slots = list(summ = "summary.nls", ib_extra = "list"))
+
+#' @rdname SummaryIb-class
 #' @export
 setClass("SummaryIbVglm",
-         slots = list(
-           summ = className("summary.vglm","VGAM"),
-           ib_extra = "list"))
+         slots = list(summ = className("summary.vglm", "VGAM"),
+                      ib_extra = "list"))
 
+## ---- Class unions (keep their own topics) ---------------------------------
 
-#' @title An S4 class union for \code{ib}
-#' @description
-#' Members of the union are \code{IbBetareg}, \code{IbGlm},
-#' \code{IbLm}, \code{IbLmer}, \code{IbNegbin},
-#' \code{IbNls}, \code{IbVglm}
-#' @details
-#' The `Functions` section describes members of the class union.
-#' @return
-#' Each member of the union has a \code{slot} with the initial object
-#' corrected by the \code{ib} (see \code{\link{getObject}}) and a second \code{slot} with
-#' extra meta data from \code{ib} (see \code{\link{getExtra}}).
-#' @author Samuel Orso
-#' @seealso \code{\link{getExtra}}, \code{\link{getObject}}
+#' An S4 class union for \code{Ib}
+#'
+#' Members: \linkS4class{IbBetareg}, \linkS4class{IbGlm},
+#' \linkS4class{IbLm}, \linkS4class{IbLmer}, \linkS4class{IbGlmer},
+#' \linkS4class{IbNegbin}, \linkS4class{IbNls}, \linkS4class{IbVglm}.
+#'
+#' @name Ib-class
+#' @aliases Ib
 #' @export
 setClassUnion(name = "Ib",
-              members = c("IbBetareg","IbGlm", "IbGlmer","IbLm","IbLmer",
+              members = c("IbBetareg","IbGlm","IbLm","IbLmer","IbGlmer",
                           "IbNegbin","IbNls","IbVglm"))
 
-#' @title An S4 class union for \code{summary}
-#' @description
-#' Members of the union are \code{SummaryIbBetareg}, \code{SummaryIbGlm}, \code{SummaryIbLm},
-#' \code{SummaryIbLmer}, \code{SummaryIbNegbin}, \code{SummaryIbNls},
-#' \code{SummaryIbVglm}
-#' @details
-#' The `Functions` section describes members of the class union.
-#' @author Samuel Orso
+#' An S4 class union for \code{SummaryIb}
+#'
+#' Members: \linkS4class{SummaryIbBetareg}, \linkS4class{SummaryIbGlm},
+#' \linkS4class{SummaryIbLm}, \linkS4class{SummaryIbLmer},
+#' \linkS4class{SummaryIbGlmer}, \linkS4class{SummaryIbNegbin},
+#' \linkS4class{SummaryIbNls}, \linkS4class{SummaryIbVglm}.
+#'
+#' @name SummaryIb-class
+#' @aliases SummaryIb
 #' @export
 setClassUnion(name = "SummaryIb",
-              members = c("SummaryIbBetareg", "SummaryIbGlm", "SummaryIbGlmer","SummaryIbLm","SummaryIbLmer",
+              members = c("SummaryIbBetareg","SummaryIbGlm","SummaryIbLm",
+                          "SummaryIbLmer","SummaryIbGlmer",
                           "SummaryIbNegbin","SummaryIbNls","SummaryIbVglm"))
